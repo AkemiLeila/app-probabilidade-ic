1bcb1993e67eede62cb623a0d1fe39692f476baa
Leila KawanishiAkemiLeila <leilaapk@gmail.com>
2026-07-05 15:49:50 -0300

    simukulação

--- app-probabilidade-ic/app_experimentos/arquivos_auxiliares/exp_moeda_quasecerta.R
+++ app-probabilidade-ic/app_experimentos/arquivos_auxiliares/exp_moeda_quasecerta.R
@@ -1,5 +1,74 @@
 exp_moeda_cqc <- function(input, output, session){
   
+  
+  
+  dados_cqc <- reactiveVal(NULL)
+  
+  
+ 
+  
+  observeEvent(input$nova_simulacao, {
+    
+    trajetorias <- replicate(6,
+                             {
+                               lancamentos <- rbinom(n = 1000, size = 1, prob = 0.5)
+                               caras_acumulada <- cumsum(lancamentos)
+                               frequencia <- caras_acumulada/seq_along(lancamentos)
+                               frequencia
+                             })
+     dados_cqc(
+      list(
+        n = seq_len(1000),
+        trajetorias = trajetorias)
+    )
+    
+  })
+  
+  
+  
+  #outputs
+  
+  output$grafico_cqc <- renderPlot({
+    
+    req(dados_cqc())
+    
+    df <- dados_cqc()
+    cores <- c(
+      "#2F5D50", # verde institucional
+      "#2E4F73", # azul institucional
+      "#A4804C", # dourado
+      "#67161C", # vinho
+      "#7B2CBF", # roxo
+      "#D95F02"  # laranja queimado
+    )
+    
+    matplot(
+      x = df$n,
+      y = df$trajetorias,
+      type = "l",
+      col = cores,
+      lty = 1,
+      lwd = 2,
+      ylim = c(0, 1),
+      xlab = "Número de lançamentos",
+      ylab = "Frequência acumulada de caras",
+      main = "Convergência Quase Certa"
+    )
+    
+    abline(
+      h = 0.5,
+      col = "#67161C",
+      lty = 2,
+      lwd = 2
+    )
+    
+  })
+  
+  
+  
+  #tagList
+  
+  
   tagList(
     br(),
     
@@ -8,7 +77,7 @@ exp_moeda_cqc <- function(input, output, session){
     
     br(),
     br(),
-    plotOutput("grafico_cqc", height = "600px")
+    plotOutput(session$ns("grafico_cqc"), height = "600px")
   )
   
 }
